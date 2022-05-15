c8a94b31a4901eb717fd48d85e01802910b6f93c
Umidjon-Fullstack <umidjonyusufov14011999@gmail.com>
2022-05-16 00:19:02 +0500

    finished

---
+++ app.js
@@ -0,0 +1,8 @@
+const loader = document.querySelector(".loader");
+
+setTimeout(() => {
+    loader.style.opacity = 0;
+    setTimeout(() => {
+        loader.style.display = "none";
+    }, 500);
+}, 4000);
